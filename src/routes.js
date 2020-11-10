da795d0876ad185ddd0308b8e16dccda0a9485ef
juancarlos <juancarloslsantos94@gmail.com>
2020-11-09 23:42:48 -0300

    Changing user creation and search routes

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ const AddressController = require('./controllers/AddressController');
 const routes = Router()
 
 routes.post('/users', UsersController.create)
-routes.get('/users', UsersController.all)
+    .get('/users', UsersController.all)
+    .get('/users/:id', UsersController.find)
 
 routes.post('/address/:userId', AddressController.create)
 
