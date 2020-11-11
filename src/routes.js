2ac2a5bc046d35eae00a512f594c3095ddc2f523
juancarlos <juancarloslsantos94@gmail.com>
2020-11-10 22:18:09 -0300

    Created the routes /login and /logout

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
 const UsersController = require('./controllers/UsersController');
 const AddressController = require('./controllers/AddressController');
-const routes = Router()
+const AuthenticationController = require('./controllers/AuthenticationController');
+const routes = Router();
 
+/**
+ * Authentication
+ */
+routes.post('/login', AuthenticationController.login)
+    .post('/logout', AuthenticationController.logout);
 routes.post('/users', UsersController.create)
     .get('/users', UsersController.all)
     .get('/users/:id', UsersController.find)
