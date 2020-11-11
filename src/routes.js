71cdfe5ea0ed59d21f47b6cded23235958e87ae8
juancarlos <juancarloslsantos94@gmail.com>
2020-11-10 22:23:21 -0300

    Creation of product access routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const UsersController = require('./controllers/UsersController');
 const AddressController = require('./controllers/AddressController');
+const ProductController = require('./controllers/ProductController');
 const AuthenticationController = require('./controllers/AuthenticationController');
 const routes = Router();
 
@@ -13,6 +14,11 @@ routes.post('/users', UsersController.create)
     .get('/users', UsersController.all)
     .get('/users/:id', UsersController.find)
 
-routes.post('/address/:userId', AddressController.create)
+/**
+ * Products
+ */
+routes.post('/products', ProductController.create)
+    .get('/products/:id', ProductController.find)
+    .get('/products', ProductController.list);
 
 module.exports = routes
