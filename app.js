455ccd0ef0ec86fefdf6990f559db4f142aa6e58
juancarlos <juancarloslsantos94@gmail.com>
2021-01-26 11:28:38 -0300

    Update app.js Add comments and export module

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const express = require('express');
 const routes = require('./src/routes');
 const db = require('./src/db/connection');
 
+
 app = express();
+
+/**
+ * Enable json data format
+ */
 app.use(express.json());
+
+/**
+ * Add routes
+ */
 app.use(routes);
 
 app.listen(3000);
+
+module.exports = app;
