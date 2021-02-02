99cb5c6c61d65e83cb45f4a91786a91b08526085
juancarlos <juancarloslsantos94@gmail.com>
2021-02-02 11:30:22 -0300

    Update User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto');
+const UserPermissions = require('./UserPermission')
 
 const UserSchema = new Schema(
     {
         name: {type: String, required: true},
-        email: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
+        salt: {type: String, required: true},
         cellphone: {type: String, required: true},
         addresses: [{
             type: Schema.Types.ObjectId,
             ref: "Address"
-        }]
-    },
-    {collection: 'user'})
+        }],
+        permission: {type: String, required: true, default: UserPermissions.USER}
+    });
 
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    this.password = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+}
+
+UserSchema.methods.validPassword = function(password){
+    var password = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.password === password;
+}
+
+
+
+module.exports = mongoose.model('User', UserSchema);
