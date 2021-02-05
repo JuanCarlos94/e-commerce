81824a5ba69a957a3b4caa009a2b8ac62ba7d1c3
juancarlos <juancarloslsantos94@gmail.com>
2021-02-05 17:02:40 -0300

    Update Product model Add indexes in name and description fields Add sold field

--- src/models/Product.js
+++ src/models/Product.js
@@ -7,8 +7,11 @@ const ProductSchema = Schema({
     images: [{type: String}],
     unitValue: { type: Number, required: true},
     count: { type: Number, required: true},
+    sold: {type: Number, required: true, default: 0},
     category: {type: Schema.Types.ObjectId, ref: 'Category'},
     evaluations: [{ type: Schema.Types.ObjectId, ref: 'ProductEvaluation'}]
 });
+
+ProductSchema.index({name: 'text', description: 'text'});
 
 module.exports = mongoose.model('Product', ProductSchema);
