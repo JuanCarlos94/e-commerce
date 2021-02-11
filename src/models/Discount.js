1af85335af0a2ffba5308efed7eb6a9a2d61ea91
juancarlos <juancarloslsantos94@gmail.com>
2021-02-10 22:48:04 -0300

    Update Discount model Added fields products, expiresAt and percentage Added apply method that calculate the discount by the value pass

--- src/models/Discount.js
+++ src/models/Discount.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DiscountSchema = Schema({
-    method: {type: Enumerator, required: true},
-    value: {type: Number, required: true}
+    name: {type: String, required: true},
+    products: [{type: Schema.Types.ObjectId, ref: 'Product'}],
+    value: {type: Number, required: true},
+    expiresAt: {type: Date, required: true},
+    percentage: {type: Boolean, default: false}
 });
+
+DiscountSchema.methods.apply = function(value) {
+    if(this.percentage)
+        return value - (this.value * value);
+    return value - this.value;
+}
 
 module.exports = mongoose.model('Discount', DiscountSchema);
