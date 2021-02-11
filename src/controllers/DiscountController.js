29c982cb49ba1f04a01e0e1273a59d854c9ecb65
juancarlos <juancarloslsantos94@gmail.com>
2021-02-10 22:48:38 -0300

    Created Discount controller

---
+++ src/controllers/DiscountController.js
@@ -0,0 +1,34 @@
+const Discount = require('../models/Discount');
+const MSG = require('../config/Messages');
+const { validationResult } = require('express-validator');
+
+module.exports = {
+    async create(req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(500).json({ message: errors.array()[0].msg });
+        const discount = new Discount(req.body);
+        await discount.save((err, obj) => {
+            if (err) res.status(500).json({ message: MSG.DISCOUNT.ERROR_WHEN_CREATING });
+            return res.status(201).json(obj);
+        });
+    },
+    async list(req, res){
+        const discounts = await Discount.find();
+        return res.json(discounts);
+    },
+    async find(req, res) {
+        const discount = await Discount.findById(req.params.id);
+        return res.json(discount);
+    },
+    async update(req, res){
+        const discount = await Discount.findOneAndUpdate(req.params.id, req.body);
+        return res.status(200).json(discount);
+    },
+    async delete(req, res){
+        await Discount.deleteOne({_id: req.params.id}, (err) => {
+            if(err) return res.status(500);
+            return res.status(200).json({message: MSG.DISCOUNT.DELETED});
+        });
+    }
+}
