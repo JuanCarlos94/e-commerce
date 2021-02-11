b0f8769777951ad4fe4b87475b707f46660d686a
juancarlos <juancarloslsantos94@gmail.com>
2021-02-10 22:54:58 -0300

    Created Discounts validator

---
+++ src/validators/DiscountValidator.js
@@ -0,0 +1,14 @@
+const { body } = require('express-validator');
+const MSG = require('../config/Messages');
+
+module.exports = {
+    create() {
+        return [
+            body('name').notEmpty().withMessage(MSG.DISCOUNT.NAME_NOT_INFORMED),
+            body('products').isArray({min: 1}).withMessage(MSG.DISCOUNT.PRODUCTS_NOT_INFORMED),
+            body('value').notEmpty().withMessage(MSG.DISCOUNT.VALUE_NOT_INFORMED),
+            body('expiresAt').notEmpty().withMessage(MSG.DISCOUNT.EXPIRES_AT_NOT_INFORMED),
+            body('percentage').notEmpty().withMessage(MSG.DISCOUNT.PERCENTAGE_NOT_INFORMED)
+        ];
+    }
+}
